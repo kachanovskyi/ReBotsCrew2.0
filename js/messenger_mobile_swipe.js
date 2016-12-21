61741f7e8c3dc513142d58fed5707ed305a4d7db
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2016-12-21 10:20:08 +0200

    working commit

--- js/messenger_mobile_swipe.js
+++ js/messenger_mobile_swipe.js
@@ -2,6 +2,13 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     alert('hey, I see that you\'re on mobile!');
 }
 
+$(document).on('pageinit', function(event){
+    $(".slide-1").swiperight(function() {
+        // $.mobile.changePage("#page1");
+        alert('pageInit swipe works');
+    });
+});
+
 $(document).ready(function() {
     console.log('script working');
     $('#botUsesCard').on( "swipeleft", function() {
