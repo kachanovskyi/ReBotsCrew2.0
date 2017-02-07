40147ff5b601baf8c67343ac4f4d556e217ea3cb
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-02-07 10:55:40 +0200

    testing orientation mode and landscape mobile menu

--- js/common_scripts.js
+++ js/common_scripts.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        if (matchMedia('all and (orientation:landscape)').matches) {
-            // console.log($(window).height());
-            $('.navbar-collapse').css('min-height', $(window).height());
-            $('#navbar').css('height', $(window).height());
-            $('.nav.navbar-nav').css('overflow', 'visible');
-            $('.nav.navbar-nav').css('max-height', 'none');
+        $( window ).on( "orientationchange", function(event) {
+            alert('You entered ' + event.orientation + ' mode');
+            if(matchMedia('all and (orientation:landscape)').matches) {
+                // console.log($(window).height());
+                $('.navbar-collapse').css('min-height', $(window).height());
+                $('#navbar').css('height', $(window).height());
+                $('.nav.navbar-nav').css('overflow', 'visible');
+                $('.nav.navbar-nav').css('max-height', 'none');
 
-            var itemHeight = (($(window).height() - 50) / 5);
-            var paddingTop = ((itemHeight - 18) / 2);
-            // console.log(paddingTop);
-            $('.nav.navbar-nav.navbar-right>li>a.menu-item').each(function () {
-                $(this).css('height', itemHeight);
-                $(this).css('padding-top', paddingTop)
-            })
-        }
+                var itemHeight = (($(window).height() - 50) / 5);
+                var paddingTop = ((itemHeight - 18) / 2);
+                // console.log(paddingTop);
+                $('.nav.navbar-nav.navbar-right>li>a.menu-item').each(function () {
+                    $(this).css('height', itemHeight);
+                    $(this).css('padding-top', paddingTop)
+                })
+            }
+        });
     }
 });
