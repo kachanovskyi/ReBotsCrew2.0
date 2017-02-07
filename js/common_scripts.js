75a8440883d8043760b7c4366f32d6e385c153f5
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-02-07 11:31:56 +0200

    added recalculating menu items in landscape mode on resizing

--- js/common_scripts.js
+++ js/common_scripts.js
@@ -1,28 +1,40 @@
 $(document).ready(function () {
+    console.log($(window).height());
+    console.log($(window).innerHeight());
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         $( window ).on( "orientationchange", function(event) {
             alert('You entered ' + event.orientation + ' mode');
             if(matchMedia('all and (orientation:landscape)').matches) {
-                // console.log($(window).height());
-                $('.navbar-collapse').css('min-height', $(window).height());
-                $('#navbar').css('height', $(window).height());
-                $('.nav.navbar-nav').css('overflow', 'visible');
-                $('.nav.navbar-nav').css('max-height', 'none');
-
-                var itemHeight = (($(window).height() - 50) / 5);
-                var paddingTop = ((itemHeight - 18) / 2);
-                // console.log(paddingTop);
+                calcMenuItemHeight();
+                $(window).on("resize", function () {
+                    calcMenuItemHeight()
+                })
+            } else if(matchMedia('(max-width:575px) and (orientation:portrait)').matches) {
                 $('.nav.navbar-nav.navbar-right>li>a.menu-item').each(function () {
-                    $(this).css('height', itemHeight);
-                    $(this).css('padding-top', paddingTop)
+                    $(this).css('height', '50px');
+                    $(this).css('padding-top', '16px')
+                })
+            } else if(matchMedia('(max-width:767px) and (orientation:portrait)').matches) {
+                $('.nav.navbar-nav.navbar-right>li>a.menu-item').each(function () {
+                    $(this).css('height', '60px');
+                    $(this).css('padding-top', '21px')
                 })
             }
-            // else if(matchMedia('(max-width:575px) and (orientation:portrait)').matches) {
-            //     $('.nav.navbar-nav.navbar-right>li>a.menu-item').each(function () {
-            //         $(this).css('height', '50px');
-            //         $(this).css('padding-top', paddingTop)
-            //     })
-            // }
         });
+    }
+
+    function calcMenuItemHeight() {
+        $('.navbar-collapse').css('min-height', $(window).height());
+        $('#navbar').css('height', $(window).height());
+        $('.nav.navbar-nav').css('overflow', 'visible');
+        $('.nav.navbar-nav').css('max-height', 'none');
+
+        var itemHeight = (($(window).height() - 50) / 5);
+        var paddingTop = ((itemHeight - 18) / 2);
+        // console.log(paddingTop);
+        $('.nav.navbar-nav.navbar-right>li>a.menu-item').each(function () {
+            $(this).css('height', itemHeight);
+            $(this).css('padding-top', paddingTop)
+        })
     }
 });
