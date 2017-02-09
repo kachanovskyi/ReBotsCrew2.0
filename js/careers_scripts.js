5c99a610eec514da474eeafebd126321c7eafa28
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2017-02-09 16:13:08 +0200

    fixed bug with hiding and displaying modal xs header on resizing

--- js/careers_scripts.js
+++ js/careers_scripts.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
+    $(window).resize(function () {
+        if(matchMedia('(max-width:767px)').matches && $("#modalSalesManager").data('bs.modal').isShown) {
+            $('.modal-mobile-header').css('display', 'block');
+        } else {
+            $('.modal-mobile-header').css('display', 'none');
+        }
+    });
+    
     $('#roleSalesManager, #roleJavaDev').on("click", function () {
         if(matchMedia('(max-width:767px)').matches) {
             $('.modal-mobile-header').css('display', 'block');
         }
     });
-
     $('#modalCloseBtn').on("click", function () {
         $('#modalSalesManager, #modalJavaDev').modal('hide');
         $('.modal-mobile-header').fadeOut("fast");
+    });
+    $('#modalSalesManager, #modalJavaDev').on('hidden.bs.modal', function() {
+        $('.modal-mobile-header').css('display', 'none');
     })
 });
